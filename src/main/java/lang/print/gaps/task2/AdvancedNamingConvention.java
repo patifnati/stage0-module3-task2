e40658786430f10e9a9bb4e937bccce754a2bb91
patifnati <rzezniczekpatrycja@gmail.com>
2022-08-15 10:57:57 +0200

    task done2

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
-    private static final int ADULT_AGE = 18;
+    public static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
@@ -10,7 +10,7 @@ public class AdvancedNamingConvention {
         //some code
     }
 
-    void callByNumber(int Number) {
+    void callByNumber(int number) {
         //some code
     }
 }
