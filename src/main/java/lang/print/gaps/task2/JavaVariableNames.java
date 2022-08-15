8516175c25e7ece91c4d24c1aae8348a89d23990
patifnati <rzezniczekpatrycja@gmail.com>
2022-08-15 08:39:38 +0200

    task done

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
-        int 2morrow = 0;
-        int $money = 10;
-        int %hundred = 100;
-        int !back = 1000;
-        int _underscore = 10000;
-        int #sharp = 100000;
-        int *star = 1000000;
-        int BADEXAMPLEOFVARNAME = 1000000;
+        int zero = 0;
+        int ten = 10;
+        int hundred = 100;
+        int thousand = 1000;
+        int tenThousand = 10000;
+        int hundredThousand = 100000;
+        int million = 1000000;
+        int millionOnceAgain = 1000000;
     }
 }
